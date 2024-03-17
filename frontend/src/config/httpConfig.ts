f829ab8aec149493e100d84837c687dccb3d42f3
yuchuan <13186087857@163.com>
2024-03-17 16:45:29 +0800

    production over

--- frontend/src/config/httpConfig.ts
+++ frontend/src/config/httpConfig.ts
@@ -1,11 +1,13 @@
-
 interface BackendServerConfig {
-    [key:string]: any
+    [key: string]: any;
 }
 
 export const backendServerConfig: BackendServerConfig = {
     dev: {
-        baseURL: 'http://127.0.0.1:8000', // 请求基础地址,可根据环境自定义
+        baseURL: 'http://localhost:8000', // 请求基础地址,可根据环境自定义
+    },
+    production: {
+        baseURL: 'http://localhost:18000', // 请求基础地址,可根据环境自定义
     },
 
     useTokenAuthorization: true, // 是否开启 token 认证
